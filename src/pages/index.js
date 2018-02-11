7c521e64f29931c84c860ac7ababc645a5cdedc9
Brantley English <brantley@sodiumhalogen.com>
2018-02-10 19:26:28 -0600

    Edits to homepage styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,16 +30,13 @@ export default class IndexPage extends React.Component {
           <div class="content">
             <div class="content__skew">
               <figure>
-                <img src="http://farm5.staticflickr.com/4707/40155267022_7442c3ac3b_b.jpg" />
+                <img src="http://sodiumhalogen.com/up_be/casey-horner-487085-iJrzaCAjf4.jpg" />
               </figure>
               <div class="content__header">
                 <h1>
-                  <img src="https://files.slack.com/files-pri/T03QR7YLR-F976KJDH8/trans2.png" />
+                  <img src="http://sodiumhalogen.com/up_be/croppedGiftED-SpyRgwcM5V.png" />
                 </h1>
-                <p>
-                  We are here to connect a community need to your spiritual
-                  gift!
-                </p>
+                <p>Learn to exercise your spiritual gifts in Christ.</p>
                 <a href="#" class="main-button">
                   Serve Now
                 </a>
@@ -52,18 +49,20 @@ export default class IndexPage extends React.Component {
               <div class="about__header">
                 <h2>About</h2>
                 <p>
-                  The Oak & the Reeds A Giant Oak stood near a brook in which
-                  grew some slender Reeds. When the wind blew, the great Oak
-                  stood proudly upright with its hundred arms uplifted to the
-                  sky. But the Reeds bowed low in the wind and sang a sad and
-                  mournful song.
+                  GiftED educates Christians on how to exercise their spiritual
+                  gifts given by the Holy Spirit, as described in Romans 12 and
+                  1 Corinthians 12. Simply search for serving opportunities in
+                  your community, and filter the results by spiritual gifts.
+                  Whether you're equipped with the gift of prophesy, discernment
+                  or any of the supernatural abilities given by Christ, GiftED
+                  will find a match, allowing you to strengthen your faith and
+                  live purposefully.
                 </p>
                 <p>
-                  "Do not worry about us," replied the Reeds. "The winds do not
-                  harm us. We bow before them and so we do not break. You, in
-                  all your pride and strength, have so far resisted their blows.
-                  But the end is coming." Better to yield when it is folly to
-                  resist, than to resist stubbornly and be destroyed.
+                  Uncertain about your spiritual gifts? Take an assessment{" "}
+                  <a href="https://spiritualgiftstest.com/spiritual-gifts/">
+                    here
+                  </a>.
                 </p>
               </div>
             </div>
